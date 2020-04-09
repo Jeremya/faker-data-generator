8a148d2d8f0934f7b54b677122b76c542123e884
Pierre Sauvage <m.sauvage.pierre@gmail.com>
2020-04-09 16:43:40 +0200

    schema

--- bank-operations/schema.sh
+++ bank-operations/schema.sh
@@ -4,10 +4,6 @@ function log(){
 	echo "[`date --iso-8601=seconds`]" $@ | tee -a log/$table.log
 }
 
-RAMPUP_ROWS=1000000
-READ_ROWS=10000
-WRITE_ROWS=10000
-MIXED_ROWS=10000
 table=""
 mkdir -p log;
 for t in mouvements_valides mouvements_valides_auto_expunge mouvements_valides_sorting; do
