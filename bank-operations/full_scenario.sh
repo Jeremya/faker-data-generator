6306335fe8850d7e1c68e25d3184e2afc63b6555
Pierre Sauvage <m.sauvage.pierre@gmail.com>
2020-04-09 16:49:54 +0200

    cool

--- bank-operations/full_scenario.sh
+++ bank-operations/full_scenario.sh
@@ -13,10 +13,10 @@ for t in mouvements_valides mouvements_valides_auto_expunge mouvements_valides_s
 	for i in `seq 0 10`; do
 		log "Start WRITE #$i with ${WRITE_ROWS} rows"
 		~/nb -v run driver=cql yaml=mouvements tags=type:write dsehost=localhost table=$table cycles=$WRITE_ROWS | tee -a log/${table}_write_${i}.log
-		sleep 20
+		#sleep 20
 		log "Start READ #$i with ${READ_ROWS} rows"
 		~/nb -v run driver=cql yaml=mouvements tags=type:read dsehost=localhost table=$table cycles=$READ_ROWS | tee -a log/${table}_read_${i}.log
-		sleep 20
+		#sleep 20
 		log "Start mixed #${i} with ${MIXED_ROWS} rows"
 		~/nb -v run driver=cql yaml=mouvements tags=phase:main dsehost=localhost table=$table cycles=$MIXED_ROWS | tee -a log/${table}_mixed_${i}.log
 	done
