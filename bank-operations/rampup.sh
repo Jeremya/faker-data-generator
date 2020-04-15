b4eed7d2be75f11bd24122794ea2d9e7e6613186
Pierre Sauvage <m.sauvage.pierre@gmail.com>
2020-04-15 13:22:31 +0200

    loop

--- bank-operations/rampup.sh
+++ bank-operations/rampup.sh
@@ -4,14 +4,23 @@ function log(){
 	echo "[`date --iso-8601=seconds`]" $@ | tee -a logs/$table.log
 }
 
+HOST="10.101.33.38"
 RAMPUP_ROWS=1000000
+PAUSE="60"
+LOOP=3
+
+
 table=""
-HOST="localhost"
 mkdir -p logs;
-for t in mouvements_valides mouvements_valides_auto_expunge mouvements_valides_sorting; do
-	table=$t;
-	log starting with table $table
-	log "Start RAMPING UP with ${RAMPUP_ROWS} rows"
-	~/nb -v --docker-metrics run driver=cql yaml=mouvements tags=phase:rampup host=${HOST} table=$table cycles=$RAMPUP_ROWS threads=auto
-	log "End of ramping up for table $table"
+for i in `seq ${LOOP}`; do
+	for t in mouvements_valides mouvements_valides_auto_expunge mouvements_valides_sorting; do
+		table=$t;
+		log "----------------------------------------------"
+		log "Start #$i RAMP UP $table with ${RAMPUP_ROWS} rows"
+		~/nb -v --docker-metrics run driver=cql yaml=mouvements tags=phase:rampup host=${HOST} table=$table cycles=$RAMPUP_ROWS threads=auto
+		log "End of #$i RAMP UP for table $table"
+		log "waiting ${PAUSE} sec."
+		sleep ${PAUSE}
+	done
+	sleep ${PAUSE}
 done
