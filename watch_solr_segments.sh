9ef74122abaeda2a2066e74b5096682c54d492b0
Pierre Sauvage <m.sauvage.pierre@gmail.com>
2020-04-20 12:29:02 +0200

    watch solr

---
+++ watch_solr_segments.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+myPath=/mnt/cass_data_disks/data1/data/solr.data
+
+data=`find $myPath -maxdepth 1 -type d`;
+
+for dir in $data; do
+  printf "`basename $dir`,"
+done
+echo date
+while true; do
+  for dir in $data; do 
+     find $dir -type f -name "*.fdx" | wc -l | tr '\n' ,
+  done 
+  date -Iseconds
+  sleep 1;
+done
