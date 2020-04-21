aecf4573e2a95ced80dbe238b90804bee7ab4ebd
Pierre Sauvage <m.sauvage.pierre@gmail.com>
2020-04-21 13:57:29 +0200

    add external reporter

--- simple/main.sh
+++ simple/main.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-~/nb --docker-metrics run driver=cql yaml=simple tags=phase:main cycles=20000000 threads=10 host=10.101.34.252 policy=tiered
+~/nb --report-graphite-to 10.101.36.75:9109 run driver=cql yaml=simple tags=phase:main cycles=20000000 threads=10 host=10.101.34.252 policy=tiered
 sleep 120
-~/nb --docker-metrics run driver=cql yaml=simple tags=phase:main cycles=20000000 threads=10 host=10.101.34.252 policy=sorting
+~/nb --report-graphite-to 10.101.36.75:9109 run driver=cql yaml=simple tags=phase:main cycles=20000000 threads=10 host=10.101.34.252 policy=sorting
 sleep 120
-~/nb --docker-metrics run driver=cql yaml=simple tags=phase:main cycles=20000000 threads=10 host=10.101.34.252 policy=auto_expunge
-
+~/nb --report-graphite-to 10.101.36.75:9109 run driver=cql yaml=simple tags=phase:main cycles=20000000 threads=10 host=10.101.34.252 policy=auto_expunge
